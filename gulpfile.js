e917e3c7edc7bbf0f0c091d58acc787adf79fa6f
cipher <gmgateway@gmail.com>
2014-06-15 23:47:57 +0900

    0.0.7

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,65 @@
 var gulp = require('gulp'),
-	jade = require('gulp-jade'),
-	prettify = require('gulp-prettify'),
-	less = require('gulp-less'),
-	minifycss = require('gulp-csso'),
-	rename = require('gulp-rename'),
-	browserSync = require('browser-sync');
+  jade = require('gulp-jade'),
+  prettify = require('gulp-prettify'),
+  less = require('gulp-less'),
+  minifycss = require('gulp-csso'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync');
 
 gulp.task('init', function() {
-	gulp.src('bower/jbootstrap/less/**')
-		.pipe(gulp.dest('src/css'))
-		.pipe(gulp.src('bower/jbootstrap/fonts/**'))
-		.pipe(gulp.dest('dist/fonts'))
-		.pipe(gulp.src('bower/bootstrap-accessibility-plugin/plugins/css/**'))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(gulp.src('bower/bootstrap-accessibility-plugin/plugins/js/bootstrap-accessibility.min.js'))
-		.pipe(gulp.dest('dist/js'))
-		.pipe(gulp.src('bower/jquery/dist/jquery.min.js'))
-		.pipe(gulp.dest('dist/js'));
+  gulp.src('bower/jbootstrap/less/**')
+    .pipe(gulp.dest('src/css'))
+  gulp.src('bower/jbootstrap/fonts/**')
+    .pipe(gulp.dest('dist/fonts'))
+  gulp.src('bower/bootstrap-accessibility-plugin/plugins/css/**')
+    .pipe(gulp.dest('dist/css'))
+  gulp.src('bower/bootstrap-accessibility-plugin/plugins/js/bootstrap-accessibility.min.js')
+    .pipe(gulp.dest('dist/js'))
+  gulp.src('bower/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('bs', function() {
-	browserSync.init(null, {
-		server: {
-			baseDir: "./dist"
-		}
-	});
+  browserSync.init(null, {
+    server: {
+      baseDir: "./dist"
+    },
+            notify: false
+  });
 });
 
 gulp.task('jade', function() {
-	gulp.src('src/templates/*.jade')
-		.pipe(jade())
-		.pipe(gulp.dest('dist'))
-	// If you need prettify HTML, uncomment below 2 lines.
-	// .pipe(prettify())
-	// .pipe(gulp.dest('dist'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+  gulp.src('src/templates/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('dist'))
+  // If you need prettify HTML, uncomment below 2 lines.
+  // .pipe(prettify())
+  // .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('styles', function() {
-	gulp.src('src/css/bootstrap.less')
-		.pipe(less({
-			sourceMap: true
-		}))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('dist/css'))
-		.pipe(browserSync.reload({
-			stream: true,
-			once: true
-		}));
+  gulp.src('src/css/bootstrap.less')
+    .pipe(less({
+      sourceMap: true
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({
+      stream: true,
+      once: true
+    }));
 });
 
 gulp.task('watch', function() {
-	gulp.watch(['src/css/**'], ['styles']);
-	gulp.watch(['src/templates/**'], ['jade']);
+  gulp.watch(['src/css/**'], ['styles']);
+  gulp.watch(['src/templates/**'], ['jade']);
 });
 
 
